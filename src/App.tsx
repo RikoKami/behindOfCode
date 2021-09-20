c549ab0c620ffe549ca2b229a7d6181681297437
Riko <orglary@gmail.com>
2021-09-20 19:11:26 -0400

    feat: add container app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <main className="App">
+      <header className="App-header">
+        <h1>Estudos com React</h1>
+
+        <Link to="/useEffect">useEffect</Link>
+      </header>
+    </main>
+  );
+}
+
+export default App;
