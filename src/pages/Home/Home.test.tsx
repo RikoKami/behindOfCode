3960d9aab9d2b497246312bb6d117904d9fbf2b2
Riko <orglary@gmail.com>
2021-09-23 00:31:27 -0400

    test: change "test" to "it" :)

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Home } from ".";
 
-test("renders learn react link", () => {
+it("renders learn react link", () => {
   const { getByText } = render(<Home />, { wrapper: MemoryRouter });
 
   const linkElement = getByText(/Estudos com React/i);
