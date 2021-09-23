4be6ab2326d436a5d659b9155fc245c825085814
Riko <orglary@gmail.com>
2021-09-23 00:30:11 -0400

    test: initial home page

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,8 +1,10 @@
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { Home } from ".";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<Home />);
+  const { getByText } = render(<Home />, { wrapper: MemoryRouter });
+
   const linkElement = getByText(/Estudos com React/i);
   expect(linkElement).toBeInTheDocument();
 });
